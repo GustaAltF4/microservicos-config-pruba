492747de2b0460842b5269b9e50c2f0382ba59b5
Gusta Acosta <acostag.123456@gmail.com>
2024-10-03 01:27:32 -0300

    eureka

--- usuario-microservice/src/main/java/com/usuario_microservice/UsuarioServiceApplication.java
+++ usuario-microservice/src/main/java/com/usuario_microservice/UsuarioServiceApplication.java
@@ -2,11 +2,13 @@ package com.usuario_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
